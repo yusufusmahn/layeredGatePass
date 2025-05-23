0995068cad536c81bec6989f71dd66319f10fc57
yusufusman <yusuf.usman1003@gmail.com>
2025-05-23 21:16:42 +0100

    update

--- src/data/models/Visitor.java
+++ src/data/models/Visitor.java
@@ -1,10 +1,52 @@
 package data.models;
 
 public class Visitor {
+//  private Resident whomToSee;
     private int id;
-    private String name;
+    private String fullName;
     private String phoneNumber;
     private String email;
-    private Resident whomToSee;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(o instanceof Visitor comparedVisitor) {
+        return this.id == comparedVisitor.getId();
+        }
+        return false;
+    }
+
 
 }
