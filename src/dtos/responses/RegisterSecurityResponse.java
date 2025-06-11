f69d30db333cb1f52dd9d6e7dd8e30ab147499bf
yusufusman <yusuf.usman1003@gmail.com>
2025-06-11 17:41:24 +0100

    update

---
+++ src/dtos/responses/RegisterSecurityResponse.java
@@ -0,0 +1,31 @@
+package dtos.responses;
+
+public class RegisterSecurityResponse {
+    private int securityId;
+    private String fullName;
+    private String message;
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(int securityId) {
+        this.securityId = securityId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
