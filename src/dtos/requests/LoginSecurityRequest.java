15f8e31bef369d63976ac7fdb7caff69f44baa1c
yusufusman <yusuf.usman1003@gmail.com>
2025-06-11 11:19:23 +0100

    update

---
+++ src/dtos/requests/LoginSecurityRequest.java
@@ -0,0 +1,27 @@
+package dtos.requests;
+
+public class LoginSecurityRequest {
+    private String email;
+    private String password;
+
+    public LoginSecurityRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
