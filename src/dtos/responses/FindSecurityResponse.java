c2e85836d05e0efc73d672189a431811fd624dd9
yusufusman <yusuf.usman1003@gmail.com>
2025-06-11 09:15:19 +0100

    update

---
+++ src/dtos/responses/FindSecurityResponse.java
@@ -0,0 +1,40 @@
+package dtos.responses;
+
+public class FindSecurityResponse {
+    private int securityId;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(int securityId) {
+        this.securityId = securityId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/dtos/responses/FindVisitorResponse.java
@@ -0,0 +1,40 @@
+package dtos.responses;
+
+public class FindVisitorResponse {
+    private int visitorId;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+
+    public int getVisitorId() {
+        return visitorId;
+    }
+
+    public void setVisitorId(int visitorId) {
+        this.visitorId = visitorId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
