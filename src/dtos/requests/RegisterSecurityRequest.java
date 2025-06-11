6e986fa4cd007efaa6b9440a417c536a2eed92d0
yusufusman <yusuf.usman1003@gmail.com>
2025-06-11 16:37:25 +0100

    update

---
+++ src/dtos/requests/RegisterSecurityRequest.java
@@ -0,0 +1,40 @@
+package dtos.requests;
+
+public class RegisterSecurityRequest {
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
