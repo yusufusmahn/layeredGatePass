7875ce9b5e1aa8076b51d0d2f385b93037ac6958
yusufusman <yusuf.usman1003@gmail.com>
2025-06-11 14:39:31 +0100

    update

---
+++ src/dtos/responses/GenerateAccessCodeResponse.java
@@ -0,0 +1,31 @@
+package dtos.responses;
+
+public class GenerateAccessCodeResponse {
+    private boolean success;
+    private String message;
+    private String accessCode;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAccessCode() {
+        return accessCode;
+    }
+
+    public void setAccessCode(String accessCode) {
+        this.accessCode = accessCode;
+    }
+}
