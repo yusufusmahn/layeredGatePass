e9ec44ee3f43507045cb21c65fe01cbc72a003d7
yusufusman <yusuf.usman1003@gmail.com>
2025-06-12 16:06:04 +0100

    update

---
+++ src/dtos/requests/VerifyAccessCodeRequest.java
@@ -0,0 +1,22 @@
+package dtos.requests;
+
+public class VerifyAccessCodeRequest {
+    private int securityId;
+    private String accessCode;
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(int securityId) {
+        this.securityId = securityId;
+    }
+
+    public String getAccessCode() {
+        return accessCode;
+    }
+
+    public void setAccessCode(String accessCode) {
+        this.accessCode = accessCode;
+    }
+}
